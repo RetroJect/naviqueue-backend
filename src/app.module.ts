69d216dd1bc589c63691780c2621834598b66dde
Grayson Doshier <grayson.doshier@gmail.com>
2026-02-21 16:06:23 -0800

    feat: setup global config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import Joi from 'joi';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      cache: true,
+      validationSchema: Joi.object({
+        TWITCH_CLIENT_ID: Joi.string().required(),
+        TWITCH_CLIENT_SECRET: Joi.string().required(),
+        PORT: Joi.number().port().default(3000),
+      }),
+    }),
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
