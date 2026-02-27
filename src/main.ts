54da03cf75ca2a04952df4e9f5a7197b5b65994b
Grayson Doshier <grayson.doshier@gmail.com>
2026-02-26 21:20:54 -0800

    feat: add proxy configuration

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: {
       origin: (process.env?.CORS_DOMAINS ?? 'localhost')
         .split(',')
         .map((host) => host.trim()),
     },
   });
+
+  // Configure Express to trust available proxies
+  app.set(
+    'trust proxy',
+    (process.env?.TRUST_PROXIES ?? 'localhost')
+      .split(',')
+      .map((host) => host.trim()),
+  );
 
   // Parse request cookies
   app.use(cookieParser());
